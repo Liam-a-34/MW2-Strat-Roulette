1c6d242350b5c5c56c61f8e912c71f57c52f87f4
Liam-a-34 <liamallen343@gmail.com>
2022-12-04 15:16:31 -0600

    Added bingo

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ const wzStrats = [
     ["Vehicular Manslaughter", "The only kills you can get are with vehicles. Splat as many players as you can, and keep an eye on that gas gauge."],
     ["Support Class", "One of your teamates is the dedicated support. They are the only ones allowed to revive, pick up and hold extra armor plates, munitions boxes, stims, armor boxes, or self revives. If your support dies, you must do everything you can to get them back, or you'll spend the rest of the game without the items listed."],
     ["Fire Superiority", "The only weapons your team is allowed to carry are lmgs. All of your squad must shoot at the same time when engaging enemies. Keep their heads down!"],
-    ["Al Mazraa's Anthem", "You may only use explosives and AK's. If possible, scream Arab sounding words at the enemies through proximity chat."],
+    ["Al Mazrah's Anthem", "You may only use explosives and AK's. If possible, scream Arab sounding words at the enemies through proximity chat."],
     ["No Callouts", "No pings, no callouts. If you see an enemy, you must shout OVER THERE!"],
     ["Choo Chooooo", "You must land on the train, and hold it for as long as possible (Preferably the whole game). You may not drive any other vehicles. If you fall off, and can't get back on, you lose."],
     ["Short Bus", "Find a cargo truck, and drive around the map attempting to recruit people to your pleasure cruise. Ms. Frizzle has nothing on you."],
@@ -36,7 +36,7 @@ const wzStrats = [
     ["Vape Nation", "Find some smoke grenades. In order to engage enemies, you must throw a smoke at them and use it as cover to attack. If they're in a building, smoke the whole dang thing."],
     ["Not the IRS...", "One of the squad is the designated tax collector. They must take all of your money, and spend it how they think fit, or they can throw it away. Choice is theirs."],
     ["Runaway Rifle", "Once you start shooting at an enemy, you may not stop firing until the magazine is empty. Choose your guns wisely."],
-    ["They're Turning the Fricking Frogs Gay!", "There's something in the water. You may not leave the rivers, or ocean for more than 30 seconds. If you have a boat, you may attempt to drive it on land. Don't know how long you'll last though."],
+    ["They're Turning the Fricking Frogs Gay!", "There's something in the water. You may not leave the rivers, or ocean for more than 30 seconds. If you have a boat, you may attempt to drive it on land. Don't know how long you'll last though. Stay wet and Wild"],
     ["GET DOWN!", "Crouch or crawl the entire game. No sprinting. Better hope you land in a good spot for the zone."],
     ["Run Forrest!!", "Never stop running. You can strafe to loot, but may never stop moving. No crouching, or proning behind cover."],
     ["Stevie Wonder", "One of your teamates must close their eyes. The rest will try to keep them alive as long as possible by giving them directions."],
@@ -54,17 +54,65 @@ const wzStrats = [
     ["Last Hope", "No one can be bought back until one player is left alive"],
     ["Opposite Day", "All of your callouts must be wildly incorrect. If they're to the right, say they're behind you. If you're running away, say you're pushing. Saying these through proximity chat will add to the fun."],
     ["Kamikaze", "Get kills by using explosives, and sacrificing yourself for the kill. Crashing helicopters in to enemies counts too."],
+    ["You Brought Protection Right?", "You can't re-armor the entire game. No plates, no problem"],
+    ["Superhero Landing", "You can never pull your parachute. Yes it's possible to survive this."],
 
 
 ]
 
+const bingoText = ["Get 5 Kills", "Get 10 Squad Kills", "Open A Loadout Drop", "Get A Knockdown With A Lethal", "Get A Roadkill", "Find A Gun With Five Attachments",
+    "Direct Hit An Enemy With A Throwable", "Destroy A Vehicle", "Crash A Helicopter", "Flip A Vehicle", "See An Enemy Get A Kill",
+    "Win Gulag", "Spend Five Thousand Dollars", "Call In A Precision or Mortar Strike", "Die to Your Own Lethal Equipment", "Disable an Enemy Vehicle",
+    "Loot an Enemy's Backpack", "Open a Rare Chest", "Fully Refuel a Vehicle", "See an Enemy Dolphin Dive", "Die to Fall Damage", "See an Enemy Return From Gulag",
+    "Kill the Jailor in Gulag", "Get a Sniper Rifle Kill", "Finish an Enemy You Did Not Down", "Capture a Blacksite", "Complete a Bounty Contract",
+    "Complete a Safecracker Contract", "Get a Launcher Kill", "Get a Hipfire Kill", "Get a Kill With a Precision or Mortar",
+    "Hear an Enemy Through Prox Chat", "Make Friends With an Enemy", "Get Roadkilled", "Drown",
+    "Get a Kill With a Shotgun", "Get a Kill in Observatory", "Get a Kill in Al Mazrah City", "Spare a Downed Enemy's Life", "Stun or Flash an Enemy",
+    "Buy Armor From a Buy Station", "Buy a Munitions From a Buy Station", "Buy a Weapon From a Buy Station", "End the Game with 0 Dollars",
+    "Buy a Backpack Upgrade", "Get a Squad Wipe", "Get 15 Squad Kills", "Get Two Road Kills", "Pick Up an Enemy in Your Vehicle", "Run Completely Out of Ammo", "Fill Your Backpack With Armor Plates",
+    "Have Every Kind of Ammo at Once", "Refill at an Ammo Depot", "Pick Up and Keep a Gas Can", "Abandon a Heavy Helo in the Sky", "Beach a Boat On a Road", "Get a Melee Kill", "Reveal an Enemy With a Snapshot",
+    "Visit 3 POIS", "Revive a Teammate", "Land in a Body of Water", "Spend the Whole Game in Water", "Get a Claymore Kill", "Make it to Top 10", "Be in First 10 to Die", "Commit a War Crime >:)",
+    "Find an Armored Vehicle", "Use a Rappel", "Find a Three Plate Vest", "Get an Execution", "Die With Over 5K", "Kill Both Enemies in Gulag", "Sink a Vehicle", "Die to a Meta Gun", "Complete an Intel Cotract",
+    "Survive a Most Wanted Contract", "Get a Vehicle on Top of a Roof", "Crawl for 100 Meters", "Clear out a POI", "Use a UAV", "Counter an Enemy's UAV", "Find a Gas Mask", "Die to Gas", "Stay in the Air for 2 Minutes",
+    "Kill an Enemy That's in a Vehicle", "Find a Gun with 3 Attachments", "Find a Gun with 2 Attachments", "Find a Gun with 1 Attachments", "Find a Gun with 4 Attachments", "Tear Gas an Enemy",
+    "Steal a Teammate's Crate", "Get a Kill With a Supressed Weapon", "Bust Through 10 Different Doors", "Spot an Enemy with a Spotter Scope", "Break All of a Car's Windows", "Drive 1k Meters", "See an Loot a Body",
+    "Steal an Enemy's Vehicle", "Hold a Funeral for a Killed Enemy", "Destroy an Enemy Helo", "Crash in to an Enemy Vehicle"]
+
+
+
+    let bingoArray = []
+
+function generateBingo(){
+
+    for(let i = 1; i < 25; i++){
+
+        let randomText = Math.floor(Math.random() * 101)
+
+        if(bingoArray.includes(randomText)){
+            randomText = Math.floor(Math.random() * 101)
+        }
+
+        console.log(bingoArray)
+
+        $(`#grid-${i}`).children("p").empty()
+        $(`#grid-${i}`).children("p").html(bingoText[randomText])
+
+        bingoArray.push(randomText)
+    }
+
+
+}
+
+generateBingo();
+
 let generatedArray = []
+
 function generateNumb(){
 
 
     $(".strat-area").empty()
 
-   let randomNumber = Math.floor(Math.random() * 53)
+   let randomNumber = Math.floor(Math.random() * 55)
 
    if(generatedArray.includes(randomNumber)){
     $(".strat-area").empty()
@@ -80,13 +128,26 @@ function generateNumb(){
 
     generatedArray.push(randomNumber)
 
-    if(generatedArray.length == 53){
+    console.log(generatedArray)
+
+    if(generatedArray.length == 55){
         generatedArray = []
        }
 
-    console.log(generatedArray)
-
 }
+
+$(".gridSquare").on("click", function(){
+
+    if($(this).hasClass("green")){
+        $(this).css("background-color", "white")
+        $(this).removeClass("green")
+    } else {
+        $(this).css("background-color", "rgb(12, 208, 12)")
+        $(this).addClass("green")
+    }
+
+
+})
 
 $(".strat-button").on("click", generateNumb)
 
